f4a0b732c565b65e5164c0aea1bef09589111c8f
Anderson Teala <andersonteala@gmail.com>
2021-04-11 17:18:11 -0300

    Update

--- yu-gi-oh-cards/src/layouts/MainLayout.vue
+++ yu-gi-oh-cards/src/layouts/MainLayout.vue
@@ -2,44 +2,11 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
-
         <q-toolbar-title>
-          Quasar App
+          <q-img style="width: 150px" src="https://www.gtnm.at/wp-content/uploads/bfi_thumb/a32dad516b6d10c5ab6ac415a297eaf0_yugioh-yugioh-clipart-logo_500-175-12ekhwr9cwpb8ltduwezv7j09s8bt55i3z2bjnit5p3jqfgw.png" />
         </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-1"
-    >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -49,58 +16,14 @@
 <script>
 import EssentialLink from 'components/EssentialLink.vue'
 
-const linksData = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
-
 export default {
   name: 'MainLayout',
-  components: { EssentialLink },
+  components: {
+    //
+  },
   data () {
     return {
-      leftDrawerOpen: false,
-      essentialLinks: linksData
+      //
     }
   }
 }
--- yu-gi-oh-cards/src/pages/Index.vue
+++ yu-gi-oh-cards/src/pages/Index.vue
@@ -1,14 +1,65 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+    <div class="q-pa-md row items-start q-gutter-md">
+      <div
+      class="my-card"
+      v-for="(card, index) in cards"
+      :key="index"
+      v-if="cards && 5 > 0 && index <= limitationList"
+      >
+        <img :src="card.card_images[0].image_url">
+
+        <div>
+          <div class="text-h6"># {{ card.id }}</div>
+          <div class="text-subtitle2">Name: {{ card.name }}</div>
+          <div class="text-subtitle2">Race: {{ card.race }}</div>
+          <div class="text-subtitle2">Type: {{ card.type }}</div>
+          <div class="text-subtitle2">Type: {{ card.desc }}</div>
+        </div>
+
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
+import { Quasar, QList, QItem, QItemSection, QItemLabel, QIntersection, Loading } from 'quasar'
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data() {
+    return {
+      cards: [],
+      limitationList: 5
+    }
+  },
+  computed: {
+    },
+  mounted() {
+    this.getCards()
+    this.updateLimitation()
+  },
+  methods: {
+    updateLimitation(limitationList){
+      if (this.limitationList == 5) {
+        this.limitationList = 5
+      }else{
+        this.limitationList = 5
+      }
+    },
+    getCards() {
+      Loading.show()
+      this.$axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php')
+      .then((response) => {
+        for (let index = 0; index < 5; index++) {
+          this.cards = response.data.data
+          console.log(this.cards)
+        }
+      }).catch((error) => {
+        console.log(error)
+      }).finally(() => {
+        Loading.hide()
+      })
+    },
+  }
 }
 </script>
